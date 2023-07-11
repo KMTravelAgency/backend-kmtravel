0c4a0af371441143ae9d393a01cbcbb15c847c7e
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-07-11 13:01:14 +0200

    setting up connection function

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const amqp = require('amqplib');
+
+var channel;
+var connection;
+
+var amqp_url = process.env.AMQP_URL || 'amqp://localhost:5672';
+
+async function connectQueue() {
+    try {
+        console.log('Connecting to amqp: ' + amqp_url);
+        channel = await amqp.connect(amqp_url)
+        connection = await connection.createChannel();
+
+        console.log('Connection established');
+
+        await channel.assertQueue("product_queue",)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send({message: "Hello from kmtravel backend!"});
 });
+
+
 
 app.listen(8080, console.log("Server is running on port, ", 8080));
